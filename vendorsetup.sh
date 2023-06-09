4f740a376c23f07f11da18dd1b9b4fdaa5fe84cf
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-09 15:29:20 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_oppo_a72-user
-add_lunch_combo twrp_oppo_a72-userdebug
-add_lunch_combo twrp_oppo_a72-eng
+add_lunch_combo omni_oppo_a72-user
+add_lunch_combo omni_oppo_a72-userdebug
+add_lunch_combo omni_oppo_a72-eng

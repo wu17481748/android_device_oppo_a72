dea6f1d88f1eed73536ea3ff334daa7c4ea72f35
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-09 14:36:11 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for alps oppo_a72 (oppo_a72)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_oppo_a72-user
+add_lunch_combo omni_oppo_a72-userdebug
+add_lunch_combo omni_oppo_a72-eng

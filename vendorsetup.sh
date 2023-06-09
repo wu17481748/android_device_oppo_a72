c84256f0430c30b26a9add405fe45c1bad75eec6
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-09 21:06:11 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,6 @@
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_oppo_a72-user
-add_lunch_combo omni_oppo_a72-userdebug
-add_lunch_combo omni_oppo_a72-eng
+COMMON_LUNCH_CHOICES omni_oppo_a72-user
+COMMON_LUNCH_CHOICES omni_oppo_a72-userdebug
+COMMON_LUNCH_CHOICES omni_oppo_a72-eng

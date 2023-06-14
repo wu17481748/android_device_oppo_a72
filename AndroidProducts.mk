ee92d6ff18298d89dadda0623448636e9779a2d5
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-15 01:00:51 +0800

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_oppo_a72.mk
+    $(LOCAL_DIR)/omni_oppo_a72.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_oppo_a72-user \
-    twrp_oppo_a72-userdebug \
-    twrp_oppo_a72-eng
+    omni_oppo_a72-user \
+    omni_oppo_a72-userdebug \
+    omni_oppo_a72-eng
---
+++ omni_oppo_a72.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from oppo_a72 device
+$(call inherit-product, device/alps/oppo_a72/device.mk)
+
+PRODUCT_DEVICE := oppo_a72
+PRODUCT_NAME := omni_oppo_a72
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := oppo6853
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-alps
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_oppo6853-user 11 RP1A.200720.011 1645294022283 release-keys"
+
+BUILD_FINGERPRINT := alps/vnd_oppo6853/oppo6853:11/RP1A.200720.011/1645294022283:user/release-keys

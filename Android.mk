cad8e4e3a9cd82d2a6bcf4ee87a249bc0a3b1b36
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-14 22:14:36 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for alps oppo6853 (oppo_a72)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),oppo_a72)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for alps oppo6853 (oppo_a72)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo twrp_oppo_a72-user
+add_lunch_combo twrp_oppo_a72-userdebug
+add_lunch_combo twrp_oppo_a72-eng

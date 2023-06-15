3a938f20a885100d8d97a3bcc89176a49d283b6a
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-15 15:36:09 +0800

    Update and rename omni_oppo_a72.mk to twrp_oppo_a72.mk

--- twrp_oppo_a72.mk
+++ twrp_oppo_a72.mk
@@ -10,10 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from oppo_a72 device
-$(call inherit-product, device/oppo/oppo_a72/device.mk)
+$(call inherit-product, device/alps/oppo_a72/device.mk)
 
 PRODUCT_DEVICE := oppo_a72
 PRODUCT_NAME := omni_oppo_a72

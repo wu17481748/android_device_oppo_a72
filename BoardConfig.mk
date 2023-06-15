f4cfd23eca8c2a8e07d26145faf2940e1d491b75
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2023-06-15 15:37:30 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/oppo/oppo_a72
+DEVICE_PATH := device/alps/oppo_a72
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
